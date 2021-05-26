69b75dbccd95f7e1a7b0b17b865831abf68e5ae7
AlexYudaev <lexx1304@gmail.com>
2021-05-26 12:20:58 +0300

    Rewrite addToCart function for localStorage

--- scripts/addtocart.js
+++ scripts/addtocart.js
@@ -1,3 +1,6 @@
+let storageProducts = JSON.parse(localStorage.getItem('productsInCart'));
+// null or []
+
 
 let addToCartButton = document.querySelector('.add_to_cart .btn_3');
 let addToCartFromBestSellers = document.querySelector('.add_to_cart_sellers');
@@ -7,19 +10,45 @@ addToCartFromBestSellers.addEventListener('click', addToCartItem);
 
 function addToCartItem(event){
     event.preventDefault();
-
+    
     // let productId = addToCartButton.dataset.productId;
     // let  {productId, title} = addToCartButton.dataset;
     let { dataset : {productId, count} } = addToCartButton;
-
+    // todo rewrite addToCartButton to addToCartFromBestSellers
 
     let productCounter = 1;
     if (count != 1){
         productCounter = document.querySelector('.product_count .input-number').value;
+    } 
+    
+    // createCartItem(productId, productCounter);   
+
+    // צריך לבדוק האם המוצר קיים בתוך הסל, במקרה שלי בתוך הדפדפן
+    // localStorage or storageProducts
+
+    // productId, productCounter
+
+    let productInCart = false;
+    storageProducts.forEach( function(item){
+        console.log(item.productId)
+        console.log(item.count);
+
+        if ( item.productId ==  parseInt(productId) ){
+            item.count = item.count + parseInt(productCounter);
+            productInCart = true;
+        }     
+    });
+
+    if (productInCart == false) {
+        storageProducts.push( { productId: parseInt(productId),  count: parseInt(productCounter) } );
     }
     
-    createCartItem(productId, productCounter);   
+    localStorage.setItem('productsInCart', JSON.stringify(storageProducts));
 }
+
+
+
+
 
 
 function createCartItem (prodictId, productCounter = 1){
@@ -62,17 +91,37 @@ function createCartItem (prodictId, productCounter = 1){
 
      let singleProductHtml = `
         <div class="single-product">
-        <div class="image">
-        <img src="${image}">
+            <div class="image">
+            <img src="${image}">
+            </div>
+        
+            <div class="details">
+            <h3>${name}</h3>
+            <h2>${price}</h2> 
+            <p>${price * productCounter}</p>
+            </div>
         </div>
-    
-        <div class="details">
-        <h3>${name}</h3>
-        <h2>${price}</h2> 
-        <p>${price * productCounter}</p>
-        </div>
-    </div>
      `;
 
-     singleProduct.innerHTML = singleProductHtml;
+     // Create array or object with productId +  count
+
+     singleProduct.innerHTML += singleProductHtml;
+
+     
 }
+
+
+
+
+// let productsInCart = [
+//     { productId : 5, count : 1},
+//     { productId : 2, count : 3 },
+//     { productId : 3, count : 1},
+//     { productId : 4, count : 2 },
+// ]
+
+
+// כל פעם כשאר מוספים לעגלת קניות מוצר אני רוצה לעדכן את  המערך בדפדפן 
+// localstorage (set or get)
+// localStorage.setItem('productsInCart', JSON.stringify(productsInCart ));
+
